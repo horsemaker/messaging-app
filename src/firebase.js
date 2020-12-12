e273b53fcbd3ac5faccca0cfc9785c85e8903548
horsemaker <yash_ghodekar@yahoo.in>
2020-12-12 17:51:16 +0530

    Final Commit.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJTBcV8KfjHeNcR4aGbmbAe-vs8_Xpgn0",
+    authDomain: "messaging-app-983cb.firebaseapp.com",
+    databaseURL: "https://messaging-app-983cb.firebaseio.com",
+    projectId: "messaging-app-983cb",
+    storageBucket: "messaging-app-983cb.appspot.com",
+    messagingSenderId: "818447271259",
+    appId: "1:818447271259:web:ed8f6c11d082a65993067e",
+    measurementId: "G-00CBD0CTH8"
+  };
